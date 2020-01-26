ada366f495eda6ba44739fa3473cf6f247e2c70e
DeveloperPrince <princekudzaimaposa94@gmail.com>
2020-01-26 14:31:01 +0200

    adjustments

--- config.py
+++ config.py
@@ -12,17 +12,5 @@ SECRET_KEY = 'theapi2019#'
 
 CSRF_ENABLED = True
 
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-
-if os.environ.get('DATABASE_URL') is None:
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'api.db')
-
-else:
-	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
-SECRET_KEY = 'theapi2019#'
-
-CSRF_ENABLED = True
 
