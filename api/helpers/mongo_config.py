756bd616b957e3f04110c5dce74ba24aff1f22c6
DeveloperPrince <princekudzaimaposa94@gmail.com>
2020-01-26 14:28:36 +0200

    adjustments

--- api/helpers/mongo_config.py
+++ api/helpers/mongo_config.py
@@ -6,7 +6,7 @@ APP_ROOT = os.path.join(os.path.dirname(__file__), '')   # refers to application
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
-mongo_uri = os.getenv('MONGODB_URI')
+mongo_uri = os.getenv('MONGODB_URI') or os.environ.get('MONGODB_URI') 
 
 class DBCONFIG():    
     client = MongoClient(mongo_uri)
