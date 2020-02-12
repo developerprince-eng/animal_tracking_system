ecb2db6c995ffb86fe36090faccdc45743df4961
DeveloperPrince <princekudzaimaposa94@gmail.com>
2020-02-12 10:11:33 +0200

    chages

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 if os.environ.get('DATABASE_URL') is None:
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'api.db')
-
+	
 else:
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
