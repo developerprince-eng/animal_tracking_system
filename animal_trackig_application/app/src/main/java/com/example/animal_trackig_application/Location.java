b10a1d006f2acf5c9376b1aeea2a7dc28ac6fc82
DeveloperPrince <princekudzaimaposa94@gmail.com>
2020-02-13 06:46:13 +0200

    android-app

---
+++ animal_trackig_application/app/src/main/java/com/example/animal_trackig_application/Location.java
@@ -0,0 +1,35 @@
+package com.example.animal_trackig_application;
+
+public class Location {
+
+    private String _id;
+    private String latitude;
+    private String longitude;
+    private String temperature;
+    private String hear_rate;
+    private String created_time;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public String getHear_rate() {
+        return hear_rate;
+    }
+
+    public String getCreated_time() {
+        return created_time;
+    }
+}
